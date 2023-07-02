fca30b4107513508b600eea0d6efff1a128ae5f3
Sergio <65853675+bassdarkside@users.noreply.github.com>
2023-07-02 13:09:05 +0000

    add user input, color output

--- request_num.py
+++ request_num.py
@@ -3,23 +3,7 @@
 '''
 import re
 import requests
-
-def find_numbers(url):
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        numbers = re.findall(r'\d+', response.text)
-        return numbers
-    return None
-
-# Example usage:
-website_url = 'https://patriot.ua'
-numbers_found = find_numbers(website_url)
-if numbers_found:
-    print("Numbers found:", numbers_found)
-else:
-    print("Failed to retrieve the website or no numbers found.")
-
+from colorama import Fore, Back
 
 def format_phone_number(phone_number):
     '''
@@ -33,11 +17,36 @@ def format_phone_number(phone_number):
         return "Invalid phone number"
 
     # Format the number as (XXX) XXX-XXXX
-    formatted_number = f"({digits[:3]}) {digits[3:6]}-{digits[6:]}"
+    # formatted_number = f"38({digits[:3]}){digits[3:6]}-{digits[6:8]}-{digits[8:]}"
+    formatted_number = f"38{digits[:3]}{digits[3:6]}{digits[6:8]}{digits[8:]}"
+
 
     return formatted_number
 
 # Example usage
-phone = "067 346-7247"
-formatted_phone = format_phone_number(phone)
-# print(formatted_phone)
+# PHONE = "673467247"
+PHONE = input("\nEnter phone number as 10-digits format\nPhone number: ")
+formatted_phone = format_phone_number(PHONE)
+# print(Fore.BLUE + "NON-formatted phone number -> ", PHONE)
+# print(Fore.GREEN + "FORMATTED  phone number -> ", formatted_phone + Fore.RESET)
+
+def find_numbers(url):
+    '''
+        Find numbers from HTTPS GET request
+    '''
+    phonenumb = formatted_phone
+    response = requests.get(url, timeout=500)
+    if response.status_code == 200:
+        numbers = re.findall(phonenumb, response.text)
+        # numbers = re.findall(r'\d+', response.text)
+        return numbers
+    return None
+
+
+# Example usage:
+WEBSITE_URL = 'https://'+ input("Enter website for search phone number like this format 'example.com': ")
+numbers_found = find_numbers(WEBSITE_URL)
+if numbers_found:
+    print(Fore.GREEN + "Number found! --> ", Fore.RED + numbers_found[0])
+else:
+    print(Fore.RED + "Failed to retrieve the website or no numbers found.")
