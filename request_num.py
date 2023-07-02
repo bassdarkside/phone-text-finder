61323449f087a6987f97e9332138ca47e0420ed8
Sergio <65853675+bassdarkside@users.noreply.github.com>
2023-07-02 10:13:02 +0000

    ass

--- request_num.py
+++ request_num.py
@@ -1,13 +1,16 @@
+'''
+    Request Number Sender
+'''
 import re
 import requests
 
 def find_numbers(url):
+
     response = requests.get(url)
     if response.status_code == 200:
         numbers = re.findall(r'\d+', response.text)
         return numbers
-    else:
-        return None
+    return None
 
 # Example usage:
 website_url = 'https://patriot.ua'
@@ -17,7 +20,11 @@ if numbers_found:
 else:
     print("Failed to retrieve the website or no numbers found.")
 
+
 def format_phone_number(phone_number):
+    '''
+        Phone number formater
+    '''
     # Remove all non-digit characters
     digits = ''.join(filter(str.isdigit, phone_number))
 
@@ -31,6 +38,6 @@ def format_phone_number(phone_number):
     return formatted_number
 
 # Example usage
-phone = "123-456-7890"
+phone = "067 346-7247"
 formatted_phone = format_phone_number(phone)
-print(formatted_phone)
+# print(formatted_phone)
