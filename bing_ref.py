dc5a2b29305af2ad3ffe643871bc8ceea7d1e1fb
Sergio <65853675+bassdarkside@users.noreply.github.com>
2023-07-04 17:06:11 +0000

    upp

---
+++ bing_ref.py
@@ -0,0 +1,162 @@
+import os
+from web_search_client import WebSearchClient
+from web_search_client.models import SafeSearch
+from azure.core.credentials import AzureKeyCredential
+
+SUBSCRIPTION_KEY = "1c5de21b71de4a0fbca851ef70335c0f"
+ENDPOINT = "https://api.bing.microsoft.com" + "/v7.0/"
+
+# SUBSCRIPTION_KEY = None
+# ENDPOINT = "https://api.bing.microsoft.com"+  "/v7.0/"
+
+
+def result_types_lookup(subscription_key):
+    """WebSearchResultTypesLookup.
+
+    This will look up a single query (Xbox) and print out name and url for first web, image, news and videos results.
+    """
+    client = WebSearchClient(AzureKeyCredential(SUBSCRIPTION_KEY))
+
+    try:
+        web_data = client.web.search(query="xbox")
+        print('Searched for Query# " Xbox "')
+
+        # WebPages
+        if web_data.web_pages.value:
+            print("Webpage Results#{}".format(len(web_data.web_pages.value)))
+
+            first_web_page = web_data.web_pages.value[0]
+            print("First web page name: {} ".format(first_web_page.name))
+            print("First web page URL: {} ".format(first_web_page.url))
+
+        else:
+            print("Didn't see any Web data..")
+
+        # Images
+        if web_data.images.value:
+            print("Image Results#{}".format(len(web_data.images.value)))
+
+            first_image = web_data.images.value[0]
+            print("First Image name: {} ".format(first_image.name))
+            print("First Image URL: {} ".format(first_image.url))
+
+        else:
+            print("Didn't see any Image..")
+
+        # News
+        if web_data.news.value:
+            print("News Results#{}".format(len(web_data.news.value)))
+
+            first_news = web_data.news.value[0]
+            print("First News name: {} ".format(first_news.name))
+            print("First News URL: {} ".format(first_news.url))
+
+        else:
+            print("Didn't see any News..")
+
+        # Videos
+        if web_data.videos.value:
+            print("Videos Results#{}".format(len(web_data.videos.value)))
+
+            first_video = web_data.videos.value[0]
+            print("First Videos name: {} ".format(first_video.name))
+            print("First Videos URL: {} ".format(first_video.url))
+
+        else:
+            print("Didn't see any Videos..")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+def web_results_with_count_and_offset(subscription_key):
+    """WebResultsWithCountAndOffset.
+
+    This will search (Best restaurants in Seattle), verify number of results and print out name and url of first result.
+    """
+
+    client = WebSearchClient(AzureKeyCredential(SUBSCRIPTION_KEY))
+
+    try:
+        web_data = client.web.search(
+            query="Best restaurants in Seattle", offset=10, count=20
+        )
+        print('Searched for Query# " Best restaurants in Seattle "')
+
+        if web_data.web_pages.value:
+            print("Webpage Results#{}".format(len(web_data.web_pages.value)))
+
+            first_web_page = web_data.web_pages.value[0]
+            print("First web page name: {} ".format(first_web_page.name))
+            print("First web page URL: {} ".format(first_web_page.url))
+
+        else:
+            print("Didn't see any Web data..")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+def web_search_with_response_filter(subscription_key):
+    """WebSearchWithResponseFilter.
+
+    This will search (Microsoft) with response filters to news and print details of news.
+    """
+
+    client = WebSearchClient(AzureKeyCredential(SUBSCRIPTION_KEY))
+
+    try:
+        web_data = client.web.search(query="Microsoft", response_filter=["News"])
+        print('Searched for Query# " Microsoft " with response filters "News"')
+
+        # News attribute since I filtered "News"
+        if web_data.news.value:
+            print("Webpage Results#{}".format(len(web_data.news.value)))
+
+            first_web_page = web_data.news.value[0]
+            print("First web page name: {} ".format(first_web_page.name))
+            print("First web page URL: {} ".format(first_web_page.url))
+
+        else:
+            print("Didn't see any Web data..")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+def web_search_with_answer_count_promote_and_safe_search(subscription_key):
+    """WebSearchWithAnswerCountPromoteAndSafeSearch.
+
+    This will search (Lady Gaga) with answerCount and promote parameters and print details of answers.
+    """
+
+    client = WebSearchClient(AzureKeyCredential(SUBSCRIPTION_KEY))
+
+    try:
+        web_data = client.web.search(
+            query="Lady Gaga",
+            answer_count=2,
+            promote=["videos"],
+            safe_search=SafeSearch.strict,  # or directly "Strict"
+        )
+        print('Searched for Query# " Lady Gaga"')
+
+        if web_data.web_pages.value:
+            print("Webpage Results#{}".format(len(web_data.web_pages.value)))
+
+            first_web_page = web_data.web_pages.value[0]
+            print("First web page name: {} ".format(first_web_page.name))
+            print("First web page URL: {} ".format(first_web_page.url))
+
+        else:
+            print("Didn't see any Web data..")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+if __name__ == "__main__":
+    result_types_lookup(SUBSCRIPTION_KEY)
+    web_results_with_count_and_offset(SUBSCRIPTION_KEY)
+    web_search_with_response_filter(SUBSCRIPTION_KEY)
+    web_search_with_answer_count_promote_and_safe_search(SUBSCRIPTION_KEY)
--- request_num.py
+++ request_num.py
@@ -8,8 +8,8 @@ from colorama import Fore
 from phone_formatter import phones_query
 
 # from azure.cognitiveservices.search.websearch import WebSearchAPI
-# from azure.cognitiveservices.search.websearch.models import SafeSearch
-# from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.search.websearch.models import SafeSearch
+from msrest.authentication import CognitiveServicesCredentials
 
 
 def find_numbers(url):
@@ -48,37 +48,34 @@ def input_querys():
     # else:
     #     print(Fore.RED + "Failed to retrieve the website or no numbers found.")
 
-
 input_querys()
-
-
 # WEBSITE_URL = 'https://'+ input(
 #           "Enter website for search phone number like this format 'example.com': ")
 # 1c5de21b71de4a0fbca851ef70335c0f
 # https://api.bing.microsoft.com/
 
 # =-=--=-=-=-==-=-=-=-=-=-=-=-=-
-# def search_string_bing(search_term, API_KEY):
-#     # Инициализация клиента Bing Search API
-#     credentials = CognitiveServicesCredentials(API_KEY)
-#     client = WebSearchAPI(credentials)
+def search_string_bing(search_term, API_KEY):
+    # Инициализация клиента Bing Search API
+    credentials = CognitiveServicesCredentials(API_KEY)
+    client = WebSearchAPI(credentials)
 
-#     # Выполнение поиска
-#     web_data = client.web.search(query=search_term, safesearch=SafeSearch.strict)
+    # Выполнение поиска
+    web_data = client.web.search(query=search_term, safesearch=SafeSearch.strict)
 
-#     # Извлечение результатов поиска
-#     results = web_data.web_pages.value
+    # Извлечение результатов поиска
+    results = web_data.web_pages.value
 
-#     # Возвращение результатов
-#     return results
+    # Возвращение результатов
+    return results
 
-# # Пример использования
-# search_term = formatted_phone
-# API_KEY = '1c5de21b71de4a0fbca851ef70335c0f'
+# Пример использования
+search_term = 'projector'
+API_KEY = '1c5de21b71de4a0fbca851ef70335c0f'
 
-# results = search_string_bing(search_term, API_KEY)
-# print("Результаты поиска в сети интернет:")
-# for result in results:
-#     print(result.name)
-#     print(Fore.GREEN + result.url)
-#     print()
+results = search_string_bing(search_term, API_KEY)
+print("Результаты поиска в сети интернет:")
+for result in results:
+    print(result.name)
+    print(Fore.GREEN + result.url)
+    print()
