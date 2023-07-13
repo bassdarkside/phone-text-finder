b38a88516b0c5b98a6d91e72d9128ee9641aa05a
Hrachov Serhii <bassdarkside@icloud.com>
2023-07-13 21:39:32 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+""" Simple flask application
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,11 +7,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Функция index возвращает обработанный шаблон index.html.
+    :return: результат вызова функции render_template("index.html").
+    """
     return render_template("index.html")
 
 
 @app.route("/about")
 def about():
+    """
+    Функция «о» предоставляет информацию о функции.
+    """
     return "About page"
 
 
