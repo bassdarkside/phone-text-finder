30809c8c38f358ef39061cc53bed5e6e2d13f6c4
Serhii <bassdarkside@gmail.com>
2024-06-27 17:02:55 +0300

    modified:   .gitignore         modified:   main.py         modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-""" Simple flask application
-"""
+from webapp import create_app
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    return "About page"
-
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
